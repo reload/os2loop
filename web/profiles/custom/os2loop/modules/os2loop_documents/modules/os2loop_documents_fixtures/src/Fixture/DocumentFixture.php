137b377b221e34d7f2e9e1bec4af7b1e7d507828
Mikkel Ricky <rimi@aarhus.dk>
2021-03-04 07:05:16 +0100

    LOOP-748: Added step-by-step fixtures

--- web/profiles/custom/os2loop/modules/os2loop_documents/modules/os2loop_documents_fixtures/src/Fixture/DocumentFixture.php
+++ web/profiles/custom/os2loop/modules/os2loop_documents/modules/os2loop_documents_fixtures/src/Fixture/DocumentFixture.php
@@ -44,8 +44,8 @@ class DocumentFixture extends AbstractFixture implements DependentFixtureInterfa
       'os2loop_documents_hc_title' => 'Important note',
       'os2loop_documents_hc_content' => [
         'value' => <<<'BODY'
-This is an <strong>important</strong> message.
-BODY,
+    This is an <strong>important</strong> message.
+    BODY,
         'format' => 'os2loop_documents_rich_text',
       ],
     ]);
@@ -60,8 +60,8 @@ BODY,
       ],
       'os2loop_documents_tai_text' => [
         'value' => <<<'BODY'
-This is the text, but there is also an image.
-BODY,
+    This is the text, but there is also an image.
+    BODY,
         'format' => 'os2loop_documents_rich_text',
       ],
     ]);
@@ -70,8 +70,98 @@ BODY,
 
     $paragraph = Paragraph::create([
       'type' => 'os2loop_documents_step_by_step',
+      'os2loop_documents_description' => [
+        'value' => <<<'BODY'
+These are the steps you need to perform.
+BODY,
+        'format' => 'os2loop_documents_rich_text',
+      ],
     ]);
+
+    $step = Paragraph::create([
+      'type' => 'os2loop_documents_step',
+      'os2loop_documents_step_text' => [
+        'value' => <<<'BODY'
+    The first step.
+    BODY,
+        'format' => 'os2loop_documents_rich_text',
+      ],
+    ]);
+    $step->save();
+    $paragraph->get('os2loop_documents_steps')->appendItem($step);
+
+    $step = Paragraph::create([
+      'type' => 'os2loop_documents_step',
+      'os2loop_documents_step_text' => [
+        'value' => <<<'BODY'
+    The second step.
+    BODY,
+        'format' => 'os2loop_documents_rich_text',
+      ],
+    ]);
+
+    $subStep = Paragraph::create([
+      'type' => 'os2loop_documents_step',
+      'os2loop_documents_step_text' => [
+        'value' => <<<'BODY'
+    The first step under the second step.
+    BODY,
+        'format' => 'os2loop_documents_rich_text',
+      ],
+    ]);
+    $subStep->save();
+    $step->get('os2loop_documents_steps')->appendItem($subStep);
+
+    $step->save();
+    $paragraph->get('os2loop_documents_steps')->appendItem($step);
+
     $paragraph->save();
+
+    $step0 = $this->createStep([
+      'os2loop_documents_step_text' => [
+        'value' => <<<'BODY'
+Step 0.
+BODY,
+        'format' => 'os2loop_documents_rich_text',
+      ],
+    ], $paragraph);
+
+    $step00 = $this->createStep([
+      'os2loop_documents_step_text' => [
+        'value' => <<<'BODY'
+Step 0.0.
+BODY,
+        'format' => 'os2loop_documents_rich_text',
+      ],
+    ], $step0);
+
+    $step000 = $this->createStep([
+      'os2loop_documents_step_text' => [
+        'value' => <<<'BODY'
+Step 0.0.0.
+BODY,
+        'format' => 'os2loop_documents_rich_text',
+      ],
+    ], $step00);
+
+    $step001 = $this->createStep([
+      'os2loop_documents_step_text' => [
+        'value' => <<<'BODY'
+Step 0.0.1.
+BODY,
+        'format' => 'os2loop_documents_rich_text',
+      ],
+    ], $step00);
+
+    $step01 = $this->createStep([
+      'os2loop_documents_step_text' => [
+        'value' => <<<'BODY'
+Step 0.1.
+BODY,
+        'format' => 'os2loop_documents_rich_text',
+      ],
+    ], $step0);
+
     $document->get('os2loop_documents_document_conte')->appendItem($paragraph);
 
     $document->save();
@@ -94,6 +184,38 @@ BODY,
    */
   public function getGroups() {
     return ['os2loop_documents'];
+  }
+
+  /**
+   * Create a step.
+   *
+   * @param array $values
+   *   The paragraph values.
+   * @param \Drupal\paragraphs\Entity\Paragraph|null $parent
+   *   The parent step.
+   *
+   * @return \Drupal\paragraphs\Entity\Paragraph
+   *   The step.
+   *
+   * @throws \Drupal\Core\Entity\EntityStorageException
+   */
+  private function createStep(array $values, Paragraph $parent = NULL): Paragraph {
+    $step = Paragraph::create($values + ['type' => 'os2loop_documents_step']);
+    $step->save();
+    if (NULL !== $parent) {
+      switch ($parent->getType()) {
+        case 'os2loop_documents_step':
+        case 'os2loop_documents_step_by_step';
+          $parent->get('os2loop_documents_steps')->appendItem($step);
+          $parent->save();
+          break;
+
+        default:
+          throw new \InvalidArgumentException(sprintf('Invalid step parent type: %s', $parent->getType()));
+      }
+    }
+
+    return $step;
   }
 
 }
