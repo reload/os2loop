8aca9385ed40478d550cf82a31e1e91be9b5e3d4
Sine <sinje@aarhus.dk>
2021-03-29 12:32:15 +0200

    LOOP-831: added bootstrap js for tabs

--- web/profiles/custom/os2loop/themes/os2loop_theme/assets/app.js
+++ web/profiles/custom/os2loop/themes/os2loop_theme/assets/app.js
@@ -8,5 +8,8 @@
 // any CSS you import will output into a single css file (app.css in this case)
 import './app.scss';
 
+// boostrap js for tabs
+import 'bootstrap/js/dist/tab';
+
 // Need jQuery? Install it with "yarn add jquery", then uncomment to import it.
 // import $ from 'jquery';
