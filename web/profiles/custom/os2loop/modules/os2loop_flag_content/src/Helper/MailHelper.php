a184d0e6bda4f24d21b62a365ff2ca2d904ded45
Sine <sinje@aarhus.dk>
2021-04-19 19:14:59 +0200

    LOOP-750: added helper class instead of code in .module

---
+++ web/profiles/custom/os2loop/modules/os2loop_flag_content/src/Helper/MailHelper.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Drupal\os2loop_flag_content\Helper;
+
+use Drupal\Core\StringTranslation\StringTranslationTrait;
+
+/**
+ * MailHelper for creating mail templates.
+ */
+class MailHelper {
+  use StringTranslationTrait;
+
+  /**
+   * Implements hook_mail().
+   */
+  public function preprocessMail($key, &$message, $params) {
+    $options = [
+      'langcode' => $message['langcode'],
+    ];
+    switch ($key) {
+      case 'flag_content':
+        $subject = $this->t('Flagged content: @reason with @title', [
+          '@title' => $params['node_title'],
+          '@reason' => $params['reason'],
+        ], $options);
+        $message['subject'] = $subject;
+        $message['body'][] = $params['message'];
+        break;
+    }
+  }
+
+}
