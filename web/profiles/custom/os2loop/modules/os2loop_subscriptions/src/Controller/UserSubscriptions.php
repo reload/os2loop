9ba9cc721073ec930cd846a68f00fcd494ca5c58
Martin Granath <martingranath@mac.local>
2021-04-27 12:35:49 +0200

    LOOP-744: Added subscriptions module

---
+++ web/profiles/custom/os2loop/modules/os2loop_subscriptions/src/Controller/UserSubscriptions.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Drupal\os2loop_subscriptions\Controller;
+
+use Drupal\Core\Controller\ControllerBase;
+
+/**
+ * Controller class for user subscriptions page.
+ */
+class UserSubscriptions extends ControllerBase {
+
+  /**
+   * Display content for user subscriptions.
+   */
+  public function content() {
+
+    return [
+      '#type' => 'markup',
+      '#theme' => 'os2loop_subscriptions_user',
+    ];
+  }
+
+}
