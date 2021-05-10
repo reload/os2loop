2836415b84983590ab4493d81380b4bf60befc17
Sine <sinje@aarhus.dk>
2021-05-10 14:26:13 +0200

    LOOP-871: added token for email message

--- web/profiles/custom/os2loop/modules/os2loop_share_with_a_friend/src/Helper/MailHelper.php
+++ web/profiles/custom/os2loop/modules/os2loop_share_with_a_friend/src/Helper/MailHelper.php
@@ -3,7 +3,6 @@
 namespace Drupal\os2loop_share_with_a_friend\Helper;
 
 use Drupal\Core\StringTranslation\StringTranslationTrait;
-use Drupal\Core\Render\BubbleableMetadata;
 use Drupal\Core\Utility\Token;
 
 /**
@@ -32,24 +31,72 @@ class MailHelper {
    * Implements hook_mail().
    */
   public function mail($key, &$message, $params) {
-    $options = [
-      'langcode' => $message['langcode'],
-    ];
     switch ($key) {
       case 'share_with_a_friend':
         $node = $params['node'];
         // @todo move this to settings
-        $template = '[current-user:name] shared the following with you: [node:title]([node:url]) with the following message';
-        $bubbleable_metadata = new BubbleableMetadata();
-        $build['#markup'] = $this->token->replace($template, ['node' => $node], [], $bubbleable_metadata);
-        $bubbleable_metadata->applyTo($build);
-
-        $subject = $this->t('Someone shared content with you!', $options);
+        $template = '[current-user:name] shared the following with you: [node:title] ([node:url]) with the following message: [os2loop_share_with_a_friend:message]';
+        $data['node'] = $node;
+        $data['message'] = $params['message'];
+        $body = $this->renderTemplate($template, $data);
+        $template = '[current-user:name] wants to share content from [site:name] with you';
+        $subject = $this->renderTemplate($template);
         $message['subject'] = $subject;
-        $message['body'][] = $build['#markup'];
-        $message['body'][] = $params['message'];
+        $message['body'][] = $body;
         break;
     }
+  }
+
+  /**
+   * Renders content of a mail.
+   */
+  public function renderTemplate($template, array $data = NULL) {
+    if (isset($data)) {
+      return $this->token->replace($template, [
+        'node' => $data['node'],
+        'message' => $data['message'],
+      ], []);
+    }
+    else {
+      return $this->token->replace($template, [], []);
+    }
+  }
+
+  /**
+   * Implements hook_tokens().
+   */
+  public function tokens($type, $tokens, array $data) {
+    $replacements = [];
+    if ($type == 'os2loop_share_with_a_friend' && !empty($data['message'])) {
+      foreach ($tokens as $name => $original) {
+        switch ($name) {
+          case 'message':
+            $replacements[$original] = $data['message'];
+            break;
+        }
+      }
+    }
+    return $replacements;
+  }
+
+  /**
+   * Implements hook_token_info().
+   */
+  public function tokenInfo() {
+    $types['os2loop_share_with_a_friend'] = [
+      'name' => $this->t('Message type'),
+    ];
+    $tokens['message'] = [
+      'name' => $this->t('Message'),
+    ];
+
+    return [
+      'types' => $types,
+      'tokens' => [
+        'os2loop_share_with_a_friend' => $tokens,
+      ],
+    ];
+
   }
 
 }
