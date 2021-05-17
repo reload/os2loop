4ca5a4e7f8159f29a3cceaa3eade41d5c20d3909
Martin Granath <martingranath@mac.local>
2021-05-17 14:56:45 +0200

    LOOP-888: Added dependency injection

---
+++ web/profiles/custom/os2loop/modules/os2loop_section_page/src/Helper/FormHelper.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Drupal\os2loop_section_page\Helper;
+
+use Drupal\Core\Form\FormStateInterface;
+
+/**
+ * Form helper.
+ */
+class FormHelper {
+
+  /**
+   * Implements hook_form_alter().
+   *
+   * Hide section page paragraph block reference options.
+   */
+  public function alterForm(array &$form, FormStateInterface $formState, string $formId) {
+    if ($formId == 'node_os2loop_section_page_edit_form' || $formId == 'node_os2loop_section_page_form') {
+      foreach ($form['os2loop_section_page_paragraph']['widget'] as $widget_key => $widget) {
+        if (is_array($widget) && is_numeric($widget_key) && isset($widget['subform'])) {
+          $form['os2loop_section_page_paragraph']['widget'][$widget_key]['subform']['os2loop_section_page_block']['widget'][0]['settings']['views_label_checkbox']['#access'] = FALSE;
+          $form['os2loop_section_page_paragraph']['widget'][$widget_key]['subform']['os2loop_section_page_block']['widget'][0]['settings']['views_label_fieldset']['#access'] = FALSE;
+          $form['os2loop_section_page_paragraph']['widget'][$widget_key]['subform']['os2loop_section_page_block']['widget'][0]['settings']['views_label_field']['#access'] = FALSE;
+          $form['os2loop_section_page_paragraph']['widget'][$widget_key]['subform']['os2loop_section_page_block']['widget'][0]['settings']['label_display']['#access'] = FALSE;
+          $form['os2loop_section_page_paragraph']['widget'][$widget_key]['subform']['os2loop_section_page_block']['widget'][0]['settings']['label']['#access'] = FALSE;
+        }
+      }
+    }
+  }
+
+}
