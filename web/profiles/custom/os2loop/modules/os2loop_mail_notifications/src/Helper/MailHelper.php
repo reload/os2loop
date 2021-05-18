f8398b1fb3048d467910d5635679145f6a6e7f3a
Mikkel Ricky <rimi@aarhus.dk>
2021-05-18 22:01:04 +0200

    LOOP-743: Added mail template

--- web/profiles/custom/os2loop/modules/os2loop_mail_notifications/src/Helper/MailHelper.php
+++ web/profiles/custom/os2loop/modules/os2loop_mail_notifications/src/Helper/MailHelper.php
@@ -61,7 +61,7 @@ class MailHelper {
     switch ($key) {
       case static::NOTIFICATION_MAIL:
         $message['headers']['Reply-To'] = $siteMail;
-        $message['headers']['Content-Type'] = 'text/html';
+        // $message['headers']['Content-Type'] = 'text/html';
         $message['headers']['From'] = sprintf('%s <%s>', $siteName, $siteMail);
         $message['subject'] = $params['subject'];
         $message['body'][] = $params['body'];
@@ -70,53 +70,39 @@ class MailHelper {
   }
 
   /**
+   * Implements hook_theme().
+   */
+  public function theme($existing, $type, $theme, $path) {
+    return [
+      'os2loop_mail_notifications_notification' => [
+        'variables' => [
+          'site' => NULL,
+          'user' => NULL,
+          'grouped_messages' => NULL,
+        ],
+      ],
+    ];
+  }
+
+  /**
    * Send notification.
    */
   public function sendNotification(User $user, array $groupedMessages) {
     $lang_code = $user->getPreferredLangcode();
 
-    $subjectTemplate = 'Notifications from {{ site.name }}';
-    $bodyTemplate = <<<'TWIG'
-<p>Hi {{ user.display_name }} ({{ user.first_name }} {{ user.last_name }}),</p>
-
-<p>
-Here are the notifications from {{ site.name }}:
-</p>
-{% for type, messages in grouped_messages %}
-    <h1>{{ type }}</h1>
-
-    {% for message in messages %}
-    <p>{{ message.text|first|raw }}</p>
-    {% endfor %}
-{% endfor %}
-
-<p>Best regards,<br/>
-{{ site.name}}</p>
-TWIG;
-
     $elements = [
       [
-        '#type' => 'inline_template',
-        '#template' => $subjectTemplate,
-        '#context' => [
-          'user' => $this->getUserValues($user),
-          'site' => $this->siteConfig->get(),
-        ],
+        '#theme' => 'os2loop_mail_notifications_notification',
+        '#user' => $user,
+        '#site' => $this->siteConfig->get(),
+        '#grouped_messages' => $groupedMessages,
       ],
     ];
-    $params['subject'] = $this->renderer->renderPlain($elements);
-    $elements = [
-      [
-        '#type' => 'inline_template',
-        '#template' => $bodyTemplate,
-        '#context' => [
-          'user' => $this->getUserValues($user),
-          'site' => $this->siteConfig->get(),
-          'grouped_messages' => $groupedMessages,
-        ],
-      ],
-    ];
-    $params['body'] = $this->renderer->renderPlain($elements);
+    $content = (string) $this->renderer->renderPlain($elements);
+
+    $parts = $this->getParts($content);
+    $params['subject'] = $parts['subject'];
+    $params['body'] = $parts['text/plain'];
 
     $result = $this->mailer->mail(Helper::MODULE, static::NOTIFICATION_MAIL, $user->getEmail(), $lang_code, $params, NULL, TRUE);
 
@@ -124,14 +110,33 @@ TWIG;
   }
 
   /**
-   * Get user values.
+   * Get parts from rendered content.
+   *
+   * Blocks are separated by lines starting with 4 dashes followed by
+   * the mime type and some more dashes.
+   *
+   * The first block is the subject.
+   *
+   * @param string $content
+   *   The content.
+   *
+   * @return array
+   *   The parts.
    */
-  private function getUserValues(User $user) {
-    return [
-      'first_name' => $user->get('os2loop_user_given_name')->getValue()[0]['value'] ?? NULL,
-      'last_name' => $user->get('os2loop_user_family_name')->getValue()[0]['value'] ?? NULL,
-      'display_name' => $user->getDisplayName(),
-    ];
+  private function getParts(string $content): array {
+    $blocks = array_map('trim',
+      preg_split('/-{4}([^-]+)-+/', $content, -1, PREG_SPLIT_NO_EMPTY | PREG_SPLIT_DELIM_CAPTURE)
+    );
+    array_unshift($blocks, 'subject');
+
+    $parts = [];
+    for ($i = 0, $iMax = count($blocks); $i < $iMax; $i += 2) {
+      $parts[$blocks[$i]] = $blocks[$i + 1];
+    }
+
+    $parts['subject'] = trim(strip_tags($parts['subject']));
+
+    return $parts;
   }
 
 }
