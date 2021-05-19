2ce90d5f71b2e32ad4a03424669781d5884e449e
Mikkel Ricky <rimi@aarhus.dk>
2021-05-19 15:27:32 +0200

    LOOP-743: Updated user fixture. Added subscription fixture.

--- web/profiles/custom/os2loop/modules/os2loop_user/modules/os2loop_user_fixtures/src/Fixture/UserFixture.php
+++ web/profiles/custom/os2loop/modules/os2loop_user/modules/os2loop_user_fixtures/src/Fixture/UserFixture.php
@@ -3,7 +3,10 @@
 namespace Drupal\os2loop_user_fixtures\Fixture;
 
 use Drupal\content_fixtures\Fixture\AbstractFixture;
+use Drupal\content_fixtures\Fixture\DependentFixtureInterface;
 use Drupal\content_fixtures\Fixture\FixtureGroupInterface;
+use Drupal\os2loop_taxonomy_fixtures\Fixture\ProfessionFixture;
+use Drupal\os2loop_taxonomy_fixtures\Fixture\SubjectFixture;
 use Drupal\user\Entity\User;
 
 /**
@@ -11,7 +14,7 @@ use Drupal\user\Entity\User;
  *
  * @package Drupal\os2loop_user_fixtures\Fixture
  */
-class UserFixture extends AbstractFixture implements FixtureGroupInterface {
+class UserFixture extends AbstractFixture implements DependentFixtureInterface, FixtureGroupInterface {
 
   /**
    * {@inheritdoc}
@@ -27,8 +30,13 @@ class UserFixture extends AbstractFixture implements FixtureGroupInterface {
       'roles' => [
         'os2loop_user_administrator',
       ],
+      'os2loop_user_given_name' => 'Admin',
+      'os2loop_user_family_name' => 'Jensen',
+      'os2loop_user_job_title' => 'Administrator',
+      'os2loop_user_place' => 'Headquarters',
     ]);
     $user->save();
+    $this->setReference('user:administrator', $user);
 
     $user = User::create([
       'uid' => 3,
@@ -40,8 +48,33 @@ class UserFixture extends AbstractFixture implements FixtureGroupInterface {
       'roles' => [
         'authenticated',
       ],
+      'os2loop_user_given_name' => 'User',
+      'os2loop_user_family_name' => 'User',
+      'os2loop_user_job_title' => 'User',
+      'os2loop_user_place' => 'The office',
+      'os2loop_user_professions' => [
+        [
+          'target_id' => $this->getReference('os2loop_profession:Administrativ medarbejder')->id(),
+        ],
+      ],
+      'os2loop_user_areas_of_expertise' => [
+        [
+          'target_id' => $this->getReference('os2loop_subject:Dokumentation')->id(),
+        ],
+      ],
     ]);
     $user->save();
+    $this->setReference('user:user', $user);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getDependencies() {
+    return [
+      ProfessionFixture::class,
+      SubjectFixture::class,
+    ];
   }
 
   /**
